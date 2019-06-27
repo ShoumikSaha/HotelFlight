fa2dbda33909e05129d10a3fca98fc997e7144ac
iamsanjaymalakar <iamsanjaymalakar@gmail.com>
2019-06-27 23:33:34 +0600

    added template and static

--- HotelFlight/HotelFlight/settings.py
+++ HotelFlight/HotelFlight/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'HotelFlight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'HotelFlight/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'HotelFlight/static'),
+]
